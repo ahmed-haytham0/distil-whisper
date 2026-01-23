62b2b1ad32e44fc5a2b3d1ff60552d739fccdd79
ahmed-haytham0 <ahmad.haytham@nadsoft.net>
2026-01-24 00:21:58 +0200

    print parms

--- training/create_student_model.py
+++ training/create_student_model.py
@@ -26,7 +26,12 @@ import numpy as np
 import torch
 from transformers import GenerationConfig, WhisperForConditionalGeneration, WhisperProcessor
 
-
+# Configure logging
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
 
 
